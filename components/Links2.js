8e0c97eeb3db24f5459e139dd848c97ea4661696
fukuhara0114 <fukuhara0114@gmail.com>
2024-02-06 22:26:02 +0900

    0021

---
+++ components/Links2.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/Home.module.css";
+
+export default function Links2() {
+  return (
+    <>
+  
+      <div>LInks2</div>
+    </>
+  );
+}
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Links from "@/components/Links";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+
+      <main >
+      <h1>about</h1>
+      <Links />
+
+      </main>
+    </>
+  );
+}
